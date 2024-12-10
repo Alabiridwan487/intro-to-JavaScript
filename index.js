905480df24803346753a4172c578297fedc20a34
kitan <alabiridolakitan487@gmail.com>
2024-12-10 12:27:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+
+//Variable Initialization
+var fullname ='Olakitan Alabi'
+var married = false
+var numberofChildren = 2
+var disability = null
+var accountNumber = undefined
+
+
+//Console.log
+console.log(fullname);
+console.log('numberofchildren');
+console.log('20+20')
+console.log('hello world')
+
+
+//string operators
+var sentence = 'i am going to be a very good software developer'
+console.log(sentence.length)
+console.log(sentence.toUpperCase())
+console.log(sentence.toLowerCase())
+
+
+//concatenation
+var favouriteTeam = 'real madrid'
+var favouritePlayer = 'Mbappe'
+var ability = 'excellent'
+var sentence2 = 'my favouritePlayer is ' + favouritePlayer + ' he plays for ' + favouriteTeam + ' his ' + ability
+console.log(sentence2)
+
+//arithmetic operators
+var x = 5
+var y = 3
+var z = 7
+console.log(z-x)
+console.log(x+y)
+console.log(y*x)
+console.log(z/y)
+console.log(z%y)
+console.log(x**y)
+
+// math operators
+console.log(Math.sqrt(144))
+console.log(Math.pow(16,8))
+console.log(Math.round(4.6))
+console.log(Math.round(4.4))
+console.log(Math.floor(4.6))
+console.log(Math.ceil(4.4))
+
+// assignment operators
+var num1 = 25
+var num2 = 35
+var num3 = 15
+
+num1 += num3
+console.log(num1)
+num2 -= num3
+num3 -= num2
+console.log(num3)
+
+// comparison operators
+
+console.log(5==='5')
+console.log((6**1)==(2*3))
+console.log(5!=6)
+console.log(5!=='5')
+console.log(100>17)
+console.log(18<=18)
+
+
+
+
+
+
